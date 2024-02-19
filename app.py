7b39fe69167e37cfb8af757cbd3711b0c2d8cfce
wlemalin <124249669+wlemalin@users.noreply.github.com>
2024-02-19 17:25:05 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -13,25 +13,39 @@ if __name__ == '__main__':
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    entry = session['entry']
+    if request.method == "POST":
+        if request.form.get('DOWN') == 'Download':
+            download_page(session['image_url'], entry, "_".join(entry)) # Use session-stored image URLs for downloading functionality
+            create_tag("_".join(entry), len(os.listdir(f'album/{"_".join(entry)}')))
+        elif request.form.get('ACCUEIL') == "Accueil":
+                tag_list = get_tags()
+                return render_template("search_form.html",
+                                tag_list = tag_list)
     tag_list = get_tags()
     return render_template('search_form.html', tag_list=tag_list)
 
 @app.route('/projet', methods=['GET', 'POST'])
 def projet():
     if request.method == "POST":
-        entry = request.form.get("url_entry").split(' ')
-        img_link = link(research(*entry))
+        session['entry'] = request.form.get("url_entry").split(' ')
+        entry = session['entry']
+        img_link = link(generate_search_link(*entry))
         num = int(request.form.get("num_entry"))
         image_url = img_link.num_list(num)
         tag_list = get_tags()
         if request.form.get('VAL') == "Search":
             session['image_url'] = image_url  # Store the list of image URLs in the session for persistence across requests
             return render_template("search_form.html", image_url=image_url, tag_list=tag_list, search=' '.join(entry), num=num)
-        elif request.form.get('DOWN') == 'Download':
-            download_page(session['image_url'], entry, "_".join(entry)) # Use session-stored image URLs for downloading functionality
-            create_tag("_".join(entry), len(os.listdir(f'album/{"_".join(entry)}')))
-    tag_list = get_tags()
     return render_template('search_form.html', tag_list=tag_list)
+
+@app.route('/models', methods = ['POST', 'GET'])
+def training():
+    if request.method == 'POST':
+        if request.form.get('TRAIN') == "Training":
+            tag_list = get_tags()
+            return render_template("train_form.html",
+                            tag_list = tag_list)
 
 @app.route('/remove-images', methods=['POST'])
 def remove_images():
